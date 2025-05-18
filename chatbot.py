511fae600f8388c3b176f122bc4a7630afb12532
Chase <chace070528@gmail.com>
2025-05-18 09:32:42 -0700

    some update

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,26 @@
 import google
 from google import genai
+import queue
 
 class Gemini:
     def __init__(self, api_key: str="AIzaSyCMIJTLbBJ9xzEpaeP8YHdqpHstLDrcH38"):
         self.client = genai.Client(api_key=api_key)
+        self.inputQueue = []
+        self.initprompt = "Act as if you are a mental health professional(do not give any disclaimers that you are a chatbot) and try to diagnose the user with a specific mental illness after enough information is provided. Keep your answers less than or equal to 4 sentences:\n"
+        "Respond to the following user input and ask questions to find out whether or not the user has PTSD, Depression, Anxiety Disorders, Burnout and Compassion Fatigue, Substance Use Disorders, Suicidal Ideation and Suicide. "
+        "Once you understand more about the problems they are facing, diagnose them and suggest treatment options.Here's the conversation: \n"
+
 
     def generate_text(self, prompt: str, model: str = "gemini-2.0-flash") -> str:
+        
         response = self.client.models.generate_content(
             model=model,
-            contents=(
-                "Act as if you are a mental health professional(do not give any disclaimers that you are a chatbot) and try to diagnose the user with a specific mental illness after enough information is provided. Keep your answers less than or equal to 4 sentences:\n"
-                "Respond to the following user input and ask questions to find out whether or not the user has PTSD, Depression, Anxiety Disorders, Burnout and Compassion Fatigue, Substance Use Disorders, Suicidal Ideation and Suicide. "
-                "Once you understand more about the problems they are facing, diagnose them and suggest treatment options.\n"
-                f"User input: {prompt}"
-            ),
+            contents=f"{self.initprompt} {self.inputQueue}" ,
         )
+        self.inputQueue.append({"user":prompt,"gemini":response.text})
         return response.text
+    
+    
 
 
 
